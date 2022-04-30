179edaa35a03b3668f19d70d7e716f55bf601b0b
tr1ggerbug <lequockhanhkt03@gmail.com>
2022-04-30 20:42:13 +0700

    config file server.js

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const dotenv = require('dotenv');
 const api = require('./src/api');
 
 dotenv.config({ path: './config.env' });
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+const DB = process.env.DATABASE.replace('<USERNAME>', process.env.DATABASE_USERNAME)
+    .replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+    .replace('<DB_NAME>', process.env.DATABASE_NAME);
+
+mongoose
+    .connect(DB, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 
@@ -11,7 +32,7 @@ app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
 
-app.use('api/v1', api);
+app.use('/api', api);
 
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, '/public/404.html'));
