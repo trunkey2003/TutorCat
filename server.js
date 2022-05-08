6e74451c61fef0b5744482a28870b5e160fac4f6
tr1ggerbug <lequockhanhkt03@gmail.com>
2022-05-08 23:22:14 +0700

    final 1

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ const corsConfig = {
 };
 
 app.use(cors(corsConfig));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use(express.urlencoded({ extended: true }));
 
 // Data sanitization against NoSQL query injection
