fc84ae2e41dd414241c0d5e7593e59090101ebb7
trunkey2003 <21521049@gm.uit.edu.vn>
2022-05-10 23:42:06 +0700

    update client

--- client/pages/index.js
+++ client/pages/index.js
@@ -70,7 +70,7 @@ const HomePage = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="h-10 w-10 mr-4 fill-current text-gray-500 opacity-75">
                   <path d="M384 112v288c0 26.51-21.49 48-48 48h-288c-26.51 0-48-21.49-48-48v-288c0-26.51 21.49-48 48-48h288C362.5 64 384 85.49 384 112zM576 127.5v256.9c0 25.5-29.17 40.39-50.39 25.79L416 334.7V177.3l109.6-75.56C546.9 87.13 576 102.1 576 127.5z"/>
                 </svg>
-                Live Video</span
+                Call Video</span
               >
 
               <span className="w-1/2 p-4 md:w-auto flex items-center"
