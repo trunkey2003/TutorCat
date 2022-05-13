41dc7bf065bdf3baf21a5a9aa63a6da1b39ad485
sheroanh <anhnguyentuan8@gmail.com>
2022-05-13 14:44:09 +0700

    Update index.jsx

--- client/pages/live/index.jsx
+++ client/pages/live/index.jsx
@@ -41,7 +41,7 @@ const checkBrowser = () => {
       style: {
         background: '#FFCC00',
         color: '#fff',
-      },
+      }
     })
   }
 }
@@ -63,7 +63,7 @@ const socket = io(`${process.env.NEXT_PUBLIC_SERVER_URL}/live`);
 export default function Index() {
   const [error, setError] = useState("");
   const [sectionModal, setSectionModal] = useState(1);
-  const [showModalCreateRoom, setShowModalCreateRoom] = u7seState(false);
+  const [showModalCreateRoom, setShowModalCreateRoom] = useState(false);
   const [userName, setUserName] = useState("");
   const [userJob, setUserJob] = useState("");
   const [language, setLanguage] = useState("");
@@ -88,13 +88,11 @@ export default function Index() {
         //dev
         if (roomID) {
           setLoading(false)
-          checkBrowser()
         };
       });
     socket.current.on("myID", (myID) => {
       setRoomID(myID);
       setLoading(false);
-      checkBrowser()
     });
     socket.current.on("update room", () => {
       console.log("update room");
@@ -549,7 +547,7 @@ export default function Index() {
             {sectionModal == 1 ? (
               <div className="font-tapestry p-4">
                 <div className="text-2xl text-center flex justify-center font-semibold mt-5">
-                  Welcome To TutorCat
+                  Welcome To Tutor Cat
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 576 512"
@@ -605,7 +603,7 @@ export default function Index() {
                   <path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM384 288H205.3l49.38 49.38c12.5 12.5 12.5 32.75 0 45.25s-32.75 12.5-45.25 0L105.4 278.6C97.4 270.7 96 260.9 96 256c0-4.883 1.391-14.66 9.398-22.65l103.1-103.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L205.3 224H384c17.69 0 32 14.33 32 32S401.7 288 384 288z" />
                 </svg>
                 <div className="text-2xl text-center flex justify-center font-semibold mt-5">
-                  Welcome To TutorCat
+                  Welcome To Tutor Cat
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 576 512"
@@ -680,7 +678,7 @@ export default function Index() {
                   <path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM384 288H205.3l49.38 49.38c12.5 12.5 12.5 32.75 0 45.25s-32.75 12.5-45.25 0L105.4 278.6C97.4 270.7 96 260.9 96 256c0-4.883 1.391-14.66 9.398-22.65l103.1-103.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L205.3 224H384c17.69 0 32 14.33 32 32S401.7 288 384 288z" />
                 </svg>
                 <div className="text-2xl text-center flex justify-center font-semibold mt-5">
-                  Welcome To TutorCat
+                  Welcome To Tutor Cat
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 576 512"
@@ -742,7 +740,7 @@ export default function Index() {
                   <path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM384 288H205.3l49.38 49.38c12.5 12.5 12.5 32.75 0 45.25s-32.75 12.5-45.25 0L105.4 278.6C97.4 270.7 96 260.9 96 256c0-4.883 1.391-14.66 9.398-22.65l103.1-103.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L205.3 224H384c17.69 0 32 14.33 32 32S401.7 288 384 288z" />
                 </svg>
                 <div className="text-2xl text-center flex justify-center font-semibold mt-5">
-                  Welcome To TutorCat
+                  Welcome To Tutor Cat
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 576 512"
@@ -889,7 +887,7 @@ export default function Index() {
             {sectionModal == 5 ? (
               <div className="font-tapestry p-4">
                 <div className="text-2xl text-center flex justify-center font-semibold mt-5">
-                  Welcome To TutorCat
+                  Welcome To Tutor Cat
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 576 512"
@@ -957,7 +955,7 @@ export default function Index() {
                   <path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM384 288H205.3l49.38 49.38c12.5 12.5 12.5 32.75 0 45.25s-32.75 12.5-45.25 0L105.4 278.6C97.4 270.7 96 260.9 96 256c0-4.883 1.391-14.66 9.398-22.65l103.1-103.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L205.3 224H384c17.69 0 32 14.33 32 32S401.7 288 384 288z" />
                 </svg>
                 <div className="text-2xl text-center flex justify-center font-semibold mt-5">
-                  Welcome To TutorCat
+                  Welcome To Tutor Cat
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 576 512"
